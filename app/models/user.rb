18b112a2328ba1e48888be65ef3158555ca8b06a
PhilipDeFraties <philipjfry338@gmail.com>
2022-11-28 21:02:03 -0700

    Adds downcase_email to User model private methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   before_validation :downcase_email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
@@ -11,4 +11,10 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :username, length: { minimum: 4 }
+
+  private
+
+  def downcase_email
+    self.email = email.downcase if email.present?
+  end
 end
