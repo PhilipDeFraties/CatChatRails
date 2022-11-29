a08ad2ca64cae123538126617a8b258d2309f84b
PhilipDeFraties <philipjfry338@gmail.com>
2022-11-28 19:09:04 -0700

    Adds email, username, and password validations to User model, also adds has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+  
+  before_validation :downcase_email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, length: { minimum: 4 }
 end
