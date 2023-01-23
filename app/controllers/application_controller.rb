1eb7c9e6d11581cd4ffa5bf279bde9e5192597ba
PhilipDeFraties <philipjfry338@gmail.com>
2023-01-22 17:19:36 -0700

    Removes all session and login helper methods from ApplicationController, adds authenticate_request method under private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,15 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  include JsonWebToken
   skip_before_action :verify_authenticity_token
-  helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!, :set_user
+  before_action :authenticate_request
 
-  def login!
-    session[:user_id] = @user.id
-  end
-
-  def logged_in?
-    !!session[:user_id]
-  end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def authorized_user?
-    @user == current_user
-  end
-
-  def logout!
-    session.clear
-  end
-
-  def set_user
-    @user = User.find_by(id: session[:user_id])
+  private
+  def authenticate_request
+    header = request.headers["Authorization"]
+    header = header.split(" ").last if header
+    decoded = jwt_decode(header)
+    @current_user = User.find(decoded[:user_id])
   end
 end
