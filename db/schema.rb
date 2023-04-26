33dd6a3bba5d78ae33bdf97a7759f0a93ec0475c
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-26 09:54:37 -0600

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_034655) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
-    t.integer "role", default: 0
+    t.integer "role"
   end
 
 end
